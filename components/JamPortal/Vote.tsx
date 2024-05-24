15607c88de59235848307c7ca47e1b24b1e0695f
Sungyup Ju <sungyupju@Sungyupui-MacBookPro.local>
2024-05-24 16:19:27 +0900

    edited vote

--- components/JamPortal/Vote.tsx
+++ components/JamPortal/Vote.tsx
@@ -2,7 +2,6 @@ import { startOfDay } from "date-fns";
 import { Button } from "../common";
 import { doc, getDoc, onSnapshot, setDoc } from "firebase/firestore";
 import { db } from "@/firebase/config";
-import { useSession } from "next-auth/react";
 import { useEffect, useState } from "react";
 import { Session } from "next-auth";
 
@@ -10,12 +9,16 @@ interface VoteProps {
   selectedDate: Date;
   loginMember: Session["user"];
   setJamdayHandler: () => void;
+  jamdays: string[];
+  cancelJamdayHandler: () => void;
 }
 
 const Vote: React.FC<VoteProps> = ({
   selectedDate,
   loginMember,
   setJamdayHandler,
+  jamdays,
+  cancelJamdayHandler,
 }) => {
   const formattedSelectedDate = startOfDay(selectedDate).toDateString();
   const [voters, setVoters] = useState<Session["user"][]>([]);
@@ -56,41 +59,50 @@ const Vote: React.FC<VoteProps> = ({
         setHasUserVoted(
           votersList.some((voter) => voter.email === loginMember.email)
         );
-
         setVoters(votersList);
       } else {
-        console.warn("voters 받아오는 중 오류");
       }
     });
     return () => unsubscribeVoters();
-  }, []);
+  }, [selectedDate, jamdays]);
 
   return (
     <div className="p-6">
-      <p>
+      <p className="font-[1rem]">
         다음 주 잼데이 가능 일자를 투표하는 곳으로, 운영진의 판단하에 확정된
         일자엔 빨간 점이 표시됩니다.
       </p>
       <div className="flex items-center justify-center gap-4">
         <Button
           backgroundColor="sub"
-          text={!hasUserVoted ? "투표하기" : "취소하기"}
+          text={!hasUserVoted ? "투표하기" : "투표취소"}
           big
           fontColor="white"
           onClick={voteHandler}
         />
         <Button
           backgroundColor="sub"
-          text="잼데이 지정"
+          text={
+            jamdays.includes(formattedSelectedDate)
+              ? "잼데이 취소"
+              : "잼데이 지정"
+          }
           big
-          onClick={setJamdayHandler}
+          onClick={
+            jamdays.includes(formattedSelectedDate)
+              ? cancelJamdayHandler
+              : setJamdayHandler
+          }
         />
       </div>
       <div className="mt-4 rounded-xl bg-borderGray p-4 ">
         <h3>투표한 사람 ({voters.length}명)</h3>
         <div className="my-4 grid grid-cols-2 sm:grid-cols-4 gap-y-6 justify-items-center items-center">
           {voters?.map((voter) => (
-            <div className="flex items-center py-1.5 px-2 rounded-2xl w-3/4 bg-backgroundGray">
+            <div
+              className="flex items-center py-1.5 px-2 rounded-2xl w-3/4 bg-backgroundGray"
+              key={voter.email}
+            >
               <img
                 className="h-10 w-10 rounded-full mr-2"
                 src={voter.image || ""}
